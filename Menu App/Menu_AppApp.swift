1368061b58bce335e3f8056c7f6658cb84bb4292
Md Rejaul Karim <rktuhinbd@gmail.com>
2024-12-02 14:47:31 +0600

    Initial Commit

---
+++ Menu App/Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu App
+//
+//  Created by Md Rejaul Karim on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
